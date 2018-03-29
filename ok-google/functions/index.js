dfb687ebe0992daf0abb28cdc2416fe909438b17
Eleni A <eleni.arvanitis@me.com>
2018-03-29 13:20:15 -0400

    grabs the user's name

--- ok-google/functions/index.js
+++ ok-google/functions/index.js
@@ -1,41 +1,31 @@
 process.env.DEBUG = 'actions-on-google:*'
+
 const App = require('actions-on-google').DialogflowApp
-const functions = require('firebase-functions');
+    , functions = require('firebase-functions')
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((req, res) => {
-//  res.send("Hello from Firebase!");
-// });
-
-
-// a. the action name from the make_name Dialogflow intent
-const NAME_ACTION = 'make_name'
+// a. the action name from the get_name Dialogflow intent
+const NAME_ACTION = 'get_name'
 
 // b. the parameters that are parsed from the make_name intent
-const COLOR_ARGUMENT = 'color'
-const NUMBER_ARGUMENT = 'number'
+const NAME_ARGUMENT = 'name'
 
-// change blobs to something else later
-exports.blobs = functions.https.onRequest((req, res) => {
-  const app = new App({req, res})
+
+exports.userName = functions.https.onRequest((req, res) => {
+  const app = new App({ req, res })
+
   console.log('Request headers: ' + JSON.stringify(req.headers))
   console.log('Request body: ' + JSON.stringify(req.body))
 
+// c. The function that grabs the user's name
+  function getName(app) {
+    let name = app.getArgument(NAME_ARGUMENT)
 
-// c. The function that generates the silly name
-  function makeName (app) {
-    let number = app.getArgument(NUMBER_ARGUMENT)
-    let color = app.getArgument(COLOR_ARGUMENT)
-    app.tell('Alright, your silly name is ' +
-      color + ' ' + number +
-      '! I hope you like it. See you next time.')
+    app.tell(`Nice to meet you ${name}. What is your current country location?`)
   }
 
-  // d. build an action map, which maps intent names to ok-google
-  let actionMap = new Map()
-  actionMap.set(NAME_ACTION, makeName)
+  // d. build an action map, which maps intent names to functions
+  const actionMap = new Map()
+  actionMap.set(NAME_ACTION, getName)
 
   app.handleRequest(actionMap)
 })
